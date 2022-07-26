e1633b3ec8f355fb5c62d56bed363c9a65df1330
Rosandymaulana <rosandy1118@gmail.com>
2022-07-26 18:53:50 +0700

    feat(pages): implementUI

---
+++ react-ui-surat/src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import Hero from '../component/Hero'
+import Activity from '../component/Activity'
+
+const LandingPage = () => {
+    return <>
+        <Navbar />
+        <Hero />
+        <Activity />
+    </>
+}
+
+export default LandingPage
