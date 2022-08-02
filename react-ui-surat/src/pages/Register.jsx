36ea2bdf754f822eb73eaafe76cf53ade3cb4e7a
naufalyukafi <naufalyukafi10@gmail.com>
2022-08-02 08:31:04 +0700

    feat(pages): integrate auth to backend

--- react-ui-surat/src/pages/Register.jsx
+++ react-ui-surat/src/pages/Register.jsx
@@ -1,50 +1,186 @@
-import React from "react";
-import Logo from '../assets/images/img-logo.png'
+import React, { useState } from "react";
+import Logo from '../assets/images/img-login.svg';
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { TextField, Button, IconButton, InputAdornment, MenuItem } from '@mui/material'
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import axios from "axios";
 
 const Register = () => {
+    const [isProcess, setIsProcess] = useState(false);
+    const [showPassword, setShowPassword] = useState(false)
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            telp: "",
+            email: "",
+            password: "",
+            jabatan: ""
+        },
+        validationSchema: yup.object({
+            name: yup
+                .string()
+                .min(2, "Name minimal 2 characters")
+                .max(100, "Maximum 100 characters")
+                .required("Name Wajib di isi"),
+            phone: yup
+                .string()
+                .min(8, "Phone minimal 8 characters")
+                .max(14, "Maximum 14 characters")
+                .required("Phone Wajib di isi"),
+            email: yup
+                .string()
+                .min(2, "Email minimal 2 characters")
+                .max(100, "Maximum 100 characters")
+                .required("Email Wajib di isi"),
+            password: yup
+                .string()
+                .min(5, "Password minimal 5 characters")
+                .max(100, "Maximum 100 characters")
+                .required("Password Wajib di isi"),
+            jabatan: yup
+                .string()
+                .required("Jabatan Wajib di isi"),
+        }),
+    });
+
+
+    const register = () => {
+        if (isProcess) return;
+        setIsProcess(true);
+        axios.post(
+            `http://localhost:8000/api/v1/auth/register`,
+            {
+                name: formik.values.name,
+                email: formik.values.email,
+                password: formik.values.password,
+                role: parseInt(formik.values.jabatan),
+                phone: formik.values.telp
+            },
+            { timeout: 1000 * 60 }
+        ).then(result => {
+            formik.resetForm()
+            setIsProcess(false);
+            alert(result.data.message)
+        }).catch(err => {
+            formik.resetForm()
+            setIsProcess(false);
+            alert(err)
+        })
+    };
+
+
     return (
         <>
             <div className="grid grid-cols-1 laptop:grid-cols-2 desktop:grid-cols-2 tablet:grid-cols-1 handphone:grid-cols-1 desktop:gap-8">
-
-                <div className="flex items-center justify-center bg-blue-600 p-4 handphone:py-10">
+                <div className="hidden laptop:block text-center bg-zinc-200 p-4 handphone:py-10">
                     <div className="">
-                        <img src={Logo} alt="" className="text=center laptop:h-60 handphone:h-36 mx-auto mb-10" />
-                        <p className="text-white text-xl font-bold">Selamat Datang di Web Admin</p>
+                        <img src={Logo} alt="Lumajang" className="mt-28 laptop:h-60 handphone:h-36 mx-auto mb-10" />
                     </div>
                 </div>
 
                 <div className="flex items-center justify-center rounded-md">
                     <div className="handphone:min-h-fit laptop:min-h-screen flex flex-col laptop:justify-center px-0 w-full handphone:justify-start">
                         <div className="desktop:w-full laptop:w-full">
-                            <h1 className="font-bold text-center text-2xl laptop:my-6 handphone:my-6">Buat Akun Anda</h1>
-                            <div className=" w-full rounded-lg divide-y ">
-                                <div className="px-5 py-7">
-                                    {/* Nama */}
-                                    <label className="font-semibold text-lg text-gray-600 pb-1 block">Nama</label>
-                                    <input type="text" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" placeholder="Masukkan Nama" />
-                                    {/* No Telp */}
-                                    <label className="font-semibold text-lg text-gray-600 pb-1 block">No.Telp</label>
-                                    <input type="number" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" placeholder="Masukkan No.Telp" />
-                                    {/* Email */}
-                                    <label className="font-semibold text-lg text-gray-600 pb-1 block">E-mail</label>
-                                    <input type="email" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" placeholder="Masukkan Email" />
-                                    {/* Password */}
-                                    <label className="font-semibold text-lg text-gray-600 pb-1 block">Password</label>
-                                    <input type="password" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" placeholder="Masukkan Password" />
-                                    {/* Jabatan*/}
-                                    <label className="font-semibold text-lg text-gray-600 pb-1 block">Jabatan</label>
-                                    <select id="jabatan" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                        <option value="Direktur">Direktur</option>
-                                        <option value="Pegawai">Pegawai</option>
-                                    </select>
+                            <div className="w-full rounded-lg divide-y ">
+                                <div className="px-5">
+                                    <form
+                                        className="flex flex-col p-5 laptop:p-8 bg-white mt-24 laptop:mt-5 rounded-md shadow"
+                                    >
+                                        <h1 className="font-semibold text-center text-2xl mb-5">Buat Akun Anda</h1>
 
-                                    <a href="" className="underline float-right text-lg mt-4">lupa password?</a>
-                                    <button type="button" className="my-6 transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
-                                        <span className="inline-block mr-2">Masuk</span>
-                                    </button>
-                                    <p className="text-lg font-semibold mt-2 pt-1 mb-0 text-center ">
-                                        Sudah memiliki akun? <a href="#!" className="text-slate-900 hover:text-blue-400 transition duration-200 ease-in-out">Masuk</a>
-                                    </p>
+                                        <div className="mb-5">
+                                            <TextField
+                                                fullWidth
+                                                autoComplete="on"
+                                                label="Name "
+                                                placeholder="Name"
+                                                name="name"
+                                                className='mt-5'
+                                                onChange={formik.handleChange}
+                                                onBlur={formik.handleBlur}
+                                                value={formik.values.name}
+                                                error={formik.touched.name && Boolean(formik.errors.name)}
+                                                helperText={formik.touched.name && formik.errors.name}
+                                            />
+                                        </div>
+                                        <div className="mb-5">
+                                            <TextField
+                                                fullWidth
+                                                autoComplete="on"
+                                                label="Email "
+                                                placeholder="email"
+                                                name="email"
+                                                className='mt-5'
+                                                onChange={formik.handleChange}
+                                                onBlur={formik.handleBlur}
+                                                value={formik.values.email}
+                                                error={formik.touched.email && Boolean(formik.errors.email)}
+                                                helperText={formik.touched.email && formik.errors.email}
+                                            />
+                                        </div>
+                                        <div className="mb-5">
+                                            <TextField
+                                                fullWidth
+                                                autoComplete="on"
+                                                label="Password "
+                                                placeholder="password"
+                                                name="password"
+                                                type={!showPassword ? "password" : "text"}
+                                                onChange={formik.handleChange}
+                                                onBlur={formik.handleBlur}
+                                                value={formik.values.password}
+                                                error={
+                                                    formik.touched.password && Boolean(formik.errors.password)
+                                                }
+                                                helperText={formik.touched.password && formik.errors.password}
+                                                InputProps={{
+                                                    endAdornment: (
+                                                        <InputAdornment position="end">
+                                                            <IconButton
+                                                                aria-label="toggle password visibility"
+                                                                onClick={() => setShowPassword(prev => !prev)}
+                                                                size="large"
+                                                            >
+                                                                {showPassword ? <Visibility /> : <VisibilityOff />}
+                                                            </IconButton>
+                                                        </InputAdornment>
+                                                    ),
+                                                }}
+                                            />
+                                        </div>
+                                        <div className="mb-5">
+                                            <TextField
+                                                fullWidth
+                                                select
+                                                autoComplete="on"
+                                                label="Jabatan "
+                                                placeholder="jabatan"
+                                                name="jabatan"
+                                                className='mt-5'
+                                                onChange={formik.handleChange}
+                                                onBlur={formik.handleBlur}
+                                                value={formik.values.jabatan}
+                                                error={formik.touched.jabatan && Boolean(formik.errors.jabatan)}
+                                                helperText={formik.touched.jabatan && formik.errors.jabatan}
+                                            >
+                                                <MenuItem value='2'>RT</MenuItem>
+                                                <MenuItem value='3'>RW</MenuItem>
+                                                <MenuItem value='4'>Kim Kegiatan</MenuItem>
+                                                <MenuItem value='5'>Aduan Masyarakat</MenuItem>
+                                                <MenuItem value='6'>Fasilitasi Pertanahan</MenuItem>
+                                            </TextField>
+                                        </div>
+                                        <Button
+                                            onClick={register}
+                                            variant="contained"
+                                            color="primary"
+                                            size="medium"
+                                            className="bg-blue-700 p-3 w-full"
+                                        >Daftar</Button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
