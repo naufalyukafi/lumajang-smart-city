c0fba94d5750f46f37be300124ada7caff72ee17
Rosandymaulana <rosandy1118@gmail.com>
2022-09-27 22:28:19 +0700

    style: edit design footer

--- react-ui-surat/src/component/Footer.jsx
+++ react-ui-surat/src/component/Footer.jsx
@@ -3,30 +3,26 @@ import Twitter from '../assets/icons/ic-twitter.png'
 import Facebook from '../assets/icons/ic-facebook.png'
 import Instagram from '../assets/icons/ic-instagram.png'
 import Youtube from '../assets/icons/ic-youtube.png'
+import icLogo from '../assets/icons/ic-footer.png'
 
 const Footer = () => {
     return (
         <footer className="bg-white rounded-lg md:px-6 md:py-8 dark:bg-gray-900 laptop:my-16 my-10">
             <div className="">
-                <div className="grid grid-cols-1 tablet:grid-cols-1 laptop:grid-cols-3 gap-4 lg:gap-8 justify-center handphone:items-center">
-                    <div className="p-4 rounded-md items-center md:justify-center handphone:text-center text-left">
-                        <p className='font-bold'>PEMERINTAH KABUPATEN LUMAJANG</p>
-                        <p>Jalan Alun - Alun Utara No.7 Lumajang</p>
-                        <p>Telp. 0334-881146-881255</p>
+                <div className="grid grid-cols-1 tablet:grid-cols-1 laptop:grid-cols-3 gap-4 lg:gap-8 justify-center handphone:items-center w-[90%] mx-auto">
+                    <div className="rounded-md flex handphone:justify-center laptop:justify-start desktop:justify-start laptop:text-left">
+                        <img src={icLogo} alt={icLogo} className="h-16" />
                     </div>
-                    <div className="p-4 rounded-md items-center md:justify-center">
-                        <p className='font-bold uppercase text-center'>Follow Us</p>
-                        <div className="flex justify-center m-3">
-                            <a href="#twitter"><img src={Twitter} alt="" className='mx-2 w-11 h-11 cursor-pointer' /></a>
-                            <a href="#instagram"><img src={Instagram} alt="" className='mx-2 w-11 h-11 cursor-pointer' /></a>
-                            <a href="#facebook"><img src={Facebook} alt="" className='mx-2 w-11 h-11 ' /></a>
-                            <a href="#youtube"><img src={Youtube} alt="" className='mx-2 w-11 h-11 ' /></a>
+                    <div className="">
+                    </div>
+                    <div className="p-4 rounded-md text-right items-center handphone:justify-center lg:text-right handphone:text-center md:text-center">
+                        <div className="flex justify-end mb-9 handphone:justify-center laptop:justify-end">
+                            <a href="#twitter"><img src={Twitter} alt="" className='mx-2 w-9 h-9' /></a>
+                            <a href="#instagram"><img src={Instagram} alt="" className='mx-2 w-9 h-9' /></a>
+                            <a href="#facebook"><img src={Facebook} alt="" className='mx-2 w-9 h-9' /></a>
+                            <a href="#youtube"><img src={Youtube} alt="" className='ml-2 w-9 h-9' /></a>
                         </div>
-                    </div>
-                    <div className="p-4 rounded-md text-right items-center md:justify-center lg:text-right handphone:text-center md:text-center">
-                        <div>Anda Pengunjung Ke</div>
-                        <div>118.780</div>
-                        <div className='font-bold mt-2'>V 2022.1</div>
+                        <div className='font-bold mt-2 handphone:text-center laptop:text-right'>Copyright © 2022 Lumajang Inc. All rights reserved.</div>
                     </div>
                 </div>
             </div>
